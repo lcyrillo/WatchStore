f9d720436a3ba19d79b661af43c8c3b2927980d6
Lucas Alonso Cyrillo <LACyrillo@apoiocdhu.sp.gov.br>
2026-03-20 17:31:19 -0300

    Implementando consumer

---
+++ Ordering.API/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using WatchStore.Contracts;
+
+namespace Ordering.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public class OrderRequest { public Guid WatchId { get; set; } public int Quantity { get; set; } }
+
+    public OrdersController(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder([FromBody] OrderRequest orderRequest)
+    {
+        await _publishEndpoint.Publish<IOrderCreatedEvent>(new
+        {
+            orderRequest.WatchId,
+            orderRequest.Quantity
+        });
+
+        return Ok(new { Message = "Pedido enviado para processamento!" });
+    }
+}
--- Ordering.API/Program.cs
+++ Ordering.API/Program.cs
@@ -13,8 +13,7 @@ builder.Services.AddMassTransit(x =>
 {
     x.UsingRabbitMq((context, cfg) =>
     {
-        cfg.Host(builder.Configuration["RabbitMQ:Host"] ?? "localhost", "/", h =>
-        {
+        cfg.Host("rabbitmq", "/", h => {
             h.Username("guest");
             h.Password("guest");
         });
